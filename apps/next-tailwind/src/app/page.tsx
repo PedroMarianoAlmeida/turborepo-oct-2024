b73dea39956f863374878d0e5aa35c6b09e933c8
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-10 22:47:13 -0400

    tailwind from app working

--- apps/next-tailwind/src/app/page.tsx
+++ apps/next-tailwind/src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {/* <Badge>NextJS Badge</Badge> */}
-        <NextTest/>
+        <p className="bg-red-600">From APP</p>
+        <NextTest />
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
