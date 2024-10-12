5e0892b2a03ae69ccfad7e8024ca4f222886f8df
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-12 18:40:19 -0400

    looks like will works now - last step is add the new package in some app

--- packages/next-tailwind2/myDist/nextTest.js
+++ packages/next-tailwind2/myDist/nextTest.js
@@ -1,5 +1,5 @@
 "use client";
 import { jsx as _jsx } from "react/jsx-runtime";
 export const NextTest = () => {
-    return (_jsx("p", { className: "bg-blue-500 text-5xl underline", style: { padding: "100px" }, children: "Next Test ABC" }));
+    return (_jsx("p", { className: "bg-red-500 text-5xl underline", style: { padding: "100px" }, children: "Next Test ABC" }));
 };
--- packages/next-tailwind2/src/components/nextTest.tsx
+++ packages/next-tailwind2/src/components/nextTest.tsx
@@ -2,7 +2,7 @@
 
 export const NextTest = () => {
   return (
-    <p className="bg-gray-500 text-5xl underline" style={{ padding: "100px" }}>
+    <p className="bg-red-500 text-5xl underline" style={{ padding: "100px" }}>
       Next Test ABC
     </p>
   );
