41e40094851190324354241d20eed4d1fc7313d4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-12 18:27:08 -0400

    create a new component and compile ts and tailwind

---
+++ packages/next-tailwind2/src/components/nextTest.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const NextTest = () => {
+  return (
+    <p className="bg-blue-500 text-5xl underline" style={{ padding: "100px" }}>
+      Next Test ABC
+    </p>
+  );
+};
