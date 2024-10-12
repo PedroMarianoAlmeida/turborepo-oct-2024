9d61d3ed0f12bd97b8369aeec22b653a9d44c900
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-12 18:28:15 -0400

    updating tailwind and the css updates too

--- packages/next-tailwind2/src/components/nextTest.tsx
+++ packages/next-tailwind2/src/components/nextTest.tsx
@@ -2,7 +2,7 @@
 
 export const NextTest = () => {
   return (
-    <p className="bg-blue-500 text-5xl underline" style={{ padding: "100px" }}>
+    <p className="bg-gray-500 text-5xl underline" style={{ padding: "100px" }}>
       Next Test ABC
     </p>
   );
